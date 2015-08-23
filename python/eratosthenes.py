598db3ef2ec85dd9c88a2d730f26df082358f3af
WolffWo <wolffwo@googlemail.com>
2015-08-23 20:16:05 +0200

    add python folder

---
+++ python/eratosthenes.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import timeit
+
+"""
+Sieve of Eratosthenes
+
+When the algorithm terminates,
+the numbers remaining in the array are
+all the primes below n.
+
+autor: wolffwo
+"""
+
+
+def sieve(n):
+    arr = []  # create array
+    for i in range(2, n):
+        arr.append(i)  # fill array with all number between 2 and N
+    for k in range(2, n):
+        for j in range(k, n):
+                if k*j in arr:
+                    arr.remove(k*j)  # remove number
+    return arr  # array with all prime numbers
+
+prime = []
+for n in range(1, 12):
+    print 'calc run-time for :' + str(pow(2, n))  # print lenght of array
+    start = timeit.default_timer()  # start measure time
+    prime = sieve(pow(2, n))  # calc prime numbers
+    stop = timeit.default_timer()  # stop measure time
+    print stop - start  # print run-time
+    print prime
